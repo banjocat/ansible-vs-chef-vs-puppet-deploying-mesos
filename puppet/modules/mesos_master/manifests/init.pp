ba07a9c90873bb38b7c54e9dd40c4af6809ad5b9
Jack Muratore <jackmuratore@gmail.com>
2016-11-25 00:14:14 -0500

    Working mesos_master puppet module

--- puppet/modules/mesos_master/manifests/init.pp
+++ puppet/modules/mesos_master/manifests/init.pp
@@ -1,2 +1,35 @@
 class mesos_master {
+    package {'zookeeper':
+        ensure => present
+    }
+    ->
+    service {'zookeeper':
+        ensure => running
+    }
+
+    file { '/etc/mesos-master/ip':
+        ensure  => present,
+        content => "$::ipaddress",
+        notify  => Service['mesos-master'],
+    }
+    ->
+    file { '/etc/mesos-master/hostname':
+        ensure  => present,
+        content => "$::ipaddress",
+        notify  => Service['mesos-master'],
+    }
+    ->
+    service { 'mesos-master':
+        ensure => running,
+    }
+
+    package {'marathon':
+        ensure => "1.1.4-1.0.514.ubuntu1404",
+    }
+    ->
+    service {'marathon':
+        ensure   => running,
+        require => Service['mesos-master'],
+    }
+
 }
