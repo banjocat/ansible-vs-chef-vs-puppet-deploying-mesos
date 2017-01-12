f4dc900bf66f2230accfdb67acab2d536f9b70e1
banjocat <jackmuratore@gmail.com>
2017-01-12 16:05:10 -0500

    Start of chef

---
+++ chef/cookbooks/mesos/metadata.rb
@@ -0,0 +1,7 @@
+name             'mesos'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures mesos'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ chef/cookbooks/mesos/recipes/default.rb
@@ -0,0 +1,71 @@
+#
+# Cookbook Name:: mesos
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+#
+
+apt_repository 'java' do
+    uri 'ppa:openjdk-r/ppa'
+    action :add
+end
+
+package 'openjdk-8-jdk' do
+    action :install
+end
+
+$common_packages = [
+      "tar",
+      "wget",
+      "git",
+      "build-essential",
+      "python-dev",
+      "libcurl4-nss-dev",
+      "libsasl2-dev",
+      "libsasl2-modules",
+      "maven",
+      "libapr1-dev",
+      "libsvn-dev",
+      "cgroup-bin",
+      "cgroup-lite",
+      "libcgroup1",
+]
+
+
+$common_packages.each do |p|
+    package p do
+        action :install
+    end
+end
+
+apt_repository 'mesos' do
+    uri 'http://repos.mesosphere.com/ubuntu'
+    keyserver 'keyserver.ubuntu.com'
+    key 'E56151BF'
+    action :add
+    components ['main']
+end
+
+package 'mesos' do
+    action :install
+end
+
+apt_repository 'docker' do
+    uri 'https://apt.dockerproject.org/repo'
+    keyserver 'hkp://ha.pool.sks-keyservers.net:80'
+    key '58118E89F3A912897C070ADBF76221572C52609D'
+    distribution 'ubuntu-trusty'
+    components ['main']
+end
+
+package 'docker-engine' do
+    action :install
+end
+
+service 'docker' do
+    action :start
+end
+
