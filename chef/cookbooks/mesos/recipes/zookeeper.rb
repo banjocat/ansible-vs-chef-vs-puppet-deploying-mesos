084cda55d34b381e23716e4ba223dff4e998891e
banjocat <jackmuratore@gmail.com>
2017-01-25 02:33:45 -0500

    Add zookeeper template for chef

--- chef/cookbooks/mesos/recipes/zookeeper.rb
+++ chef/cookbooks/mesos/recipes/zookeeper.rb
@@ -3,6 +3,14 @@ package 'zookeeper' do
     action :install
 end
 
+puts search(:node, 'roles:zookeeper')
+template '/etc/zookeeper/conf/zoo.cfg' do
+    variables({
+        :zookeepers => search(:node, 'roles:zookeeper'),
+    })
+    source 'zoo.cfg.erb'
+end
+
 service 'zookeeper' do
     action :start
 end
